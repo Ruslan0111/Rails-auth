866e5a88c2ec68fa5a8a98336170792d6b388a8e
Ruslan0111 <gadilovruslan@yandex.ru>
2022-04-12 17:19:40 +0500

    Login provided

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,6 +10,16 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def login
+    user = User.find_by_email!(user_params[:email])
+    if user.authenticate(user_params[:password])
+      token = JsonWebToken.encode(user_id: user.id)
+      render json: { message: "Logged in", token: token }, status: :created
+    else
+      render json: { message: "Unauthorized" }, status: :unauthorized
+    end
+  end
+
   private
 
     def user_params
